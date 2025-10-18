8225e712fc8ade5c924f1ea027966a0adf9312fd
KhushiG <khushig3011@gmail.com>
2025-10-19 02:04:27 +0530

    landing comps

---
+++ frontend/src/components/common/Icons.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const ArtisanIcon = () => (
+    <svg className="w-12 h-12 mb-4 text-google-red" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6.253v11.494m-9-5.747h18M5.47 7.712A8.995 8.995 0 0112 4.5a8.995 8.995 0 016.53 3.212M5.47 16.288A8.995 8.995 0 0012 19.5a8.995 8.995 0 006.53-3.212"></path><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4.5V2.25m0 19.5V19.5m-7.288-6.53H2.25m19.5 0H19.5"></path></svg>
+);
+export const BuyerIcon = () => (
+    <svg className="w-12 h-12 mb-4 text-google-blue" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+);
+export const InvestorIcon = () => (
+    <svg className="w-12 h-12 mb-4 text-google-green" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"></path></svg>
+);
+export const AmbassadorIcon = () => (
+    <svg className="w-12 h-12 mb-4 text-google-yellow" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+);
