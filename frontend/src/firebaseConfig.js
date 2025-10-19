5422787da16973c585ed0022d3d7fe0ac8fd4ab5
eugene0111 <eugenesam1105@gmail.com>
2025-10-20 02:51:34 +0530

    shhhhh

--- frontend/src/firebaseConfig.js
+++ frontend/src/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC575B-YoVSqjT6kBB-EjehSz_LoRB2ucI",
-  authDomain: "kalaghar-d2746.firebaseapp.com",
-  projectId: "kalaghar-d2746",
-  storageBucket: "kalaghar-d2746.firebasestorage.app",
-  messagingSenderId: "809890944113",
-  appId: "1:809890944113:web:07494ecc8d01b52fa837d0",
-  measurementId: "G-5T50185M49",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
