d2f71a7853fc85a474b8985e88eed8c19e110ee3
eugene0111 <eugenesam1105@gmail.com>
2025-11-01 09:41:39 +0530

    something

--- frontend/src/components/layout/InvestorLayout.jsx
+++ frontend/src/components/layout/InvestorLayout.jsx
@@ -6,7 +6,7 @@ const InvestorLayout = () => {
   return (
     <div className="font-sans bg-gray-50 min-h-screen">
       <InvestorHeader />
-      <main className="main-page-content flex-1 overflow-y-auto pt-16 pb-8"></main>
+      <main className="main-page-content flex-1 overflow-y-auto pb-8"></main>
       <Outlet />
       {}
     </div>
